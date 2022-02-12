dc883288f018199218ce43ff9092e7e8405c4e89
Destro <swanandev@gmail.com>
2022-02-12 20:18:01 +0530

    Created Dummy Data!

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
+import data from "./static/data.json";
 
 function App() {
-  const [count, setCount] = useState(0);
+  console.log(data);
 
   return (
     <div className="min-h-screen w-full flex flex-col bg-gray-800 text-gray-50">
       <header className="flex h-20 items-center justify-center container mx-auto">
         <p className="text-2xl">Interview Project - User List</p>
       </header>
-      <main className="flex-1 flex items-center justify-center container mx-auto"></main>
+      <main className="flex-1 flex items-center justify-center container mx-auto">
+        <div
+          className="flex flex-col items-center gap-2 px-2 py-1"
+          data-tooltip-target="tooltip-top"
+          data-tooltip-placement="top"
+        >
+          <img src={data[0].imageUrl} className="rounded-full" />
+          <p>{data[0].name}</p>
+        </div>
+      </main>
     </div>
   );
 }
